a4b55052e81b058e520080b9ea8ff8979acc632a
macclintx <macclintx@gmail.com>
2024-04-14 02:35:34 +0300

    second commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::latest()->paginate(5);
+
+        return view('products.index', compact('products'))->with(request()->input('page'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('products.create'); 
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //validate the input
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+
+        //create product in the database
+        Product::create(request()->all());
+
+        //redirect the user and send a friendly message
+        return redirect()->route('products.index')->with('success', 'Product created succesfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel 8 Crud Application</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css">
+        
+    </head>
+    <body>
+        <h1>Laravel 8 Crud Application</h1>
+        <div class="container">
+            <br>
+            @yield('content')
+        </div>
+    </body>
+</html>
